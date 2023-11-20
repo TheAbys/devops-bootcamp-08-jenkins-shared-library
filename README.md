1c9116ecf8ac242c0e588da096c32853395cbe43
Sebastian Müller <sebastian.mueller@syskron.com>
2023-11-20 21:38:21 +0100

    added initial README and updated buildImage script

---
+++ README.md
@@ -0,0 +1,5 @@
+# 14 - Jankins Shared Library
+
+Makes parts of pipelines reusable in different projects without copying everything.
+Copied the repository, checked out starting-code branch and updated the existing code to my own code.
+
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+    withCredentials([usernamePassword(credentialsId: "nexus-docker-repo", passwordVariable: "PASS", usernameVariable: "USER")]){
+        sh 'docker build -t 64.226.110.153:8083/java-maven-app:1.2 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin 64.226.110.153:8083'
+        sh 'docker push 64.226.110.153:8083/java-maven-app:1.2'
     }
 }
