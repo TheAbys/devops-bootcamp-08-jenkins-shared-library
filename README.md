4a8e3f9beb8c8d055b4d31f4af32fbbb65dfb5f4
Sebastian Müller <sebastian.mueller@syskron.com>
2023-11-20 22:00:00 +0100

    added some parameters to the functions

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Makes parts of pipelines reusable in different projects without copying everything.
 Copied the repository, checked out starting-code branch and updated the existing code to my own code.
 
+Global Pipeline Libraries section within Manage Jenkins => System allows to configure a specific git repository which holds the shared libaries.
+It is good practise to define a specific version of the shared libraries and not the master branch for example.
+
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: "nexus-docker-repo", passwordVariable: "PASS", usernameVariable: "USER")]){
-        sh 'docker build -t 64.226.110.153:8083/java-maven-app:1.2 .'
+        sh "docker build -t 64.226.110.153:8083/$imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin 64.226.110.153:8083'
         sh 'docker push 64.226.110.153:8083/java-maven-app:1.2'
     }
