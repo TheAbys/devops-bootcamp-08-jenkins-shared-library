4e27e1dbb9e58ac3ac3b0c7d2731bb3b3232d252
Sebastian Müller <sebastian.mueller@syskron.com>
2024-01-31 16:03:23 +0100

    fixed wrong function name to call

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
-def deploy(String username, String ipAddress, String command, String sshAgentCredentials = "ssh-agent-credentials") {
+def call(String username, String ipAddress, String command, String sshAgentCredentials = "ssh-agent-credentials") {
     sshagent([$sshAgentCredentials]) {
         sh "scp docker-compose.yaml ${username}@${ipAddress}:/home/ec2-user"
         sh "ssh -o StrictHostKeyChecking=no ${username}@${ipAddress} ${command}"
