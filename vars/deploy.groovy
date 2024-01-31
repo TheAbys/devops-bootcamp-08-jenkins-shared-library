ca37875bdb592652770e929290e8ad6650662701
Sebastian Müller <sebastian.mueller@syskron.com>
2024-01-31 15:50:25 +0100

    added usage of docker-compose

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,6 +2,7 @@
 
 def deploy(String username, String ipAddress, String command, String sshAgentCredentials = "ssh-agent-credentials") {
     sshagent([$sshAgentCredentials]) {
+        sh "scp docker-compose.yaml ${username}@${ipAddress}:/home/ec2-user"
         sh "ssh -o StrictHostKeyChecking=no ${username}@${ipAddress} ${command}"
     }
 }
