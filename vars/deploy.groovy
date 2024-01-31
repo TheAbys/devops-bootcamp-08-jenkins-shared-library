5315a55db93c33556b9bae454e0ddbe21afaa018
Sebastian Müller <sebastian.mueller@syskron.com>
2024-01-31 16:05:30 +0100

    another try

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,7 @@
 #!/user/bin/env groovy
 
 def call(String username, String ipAddress, String command, String sshAgentCredentials = "ssh-agent-credentials") {
-    sshagent([${sshAgentCredentials}]) {
+    sshagent(["${sshAgentCredentials}"]) {
         sh "scp docker-compose.yaml ${username}@${ipAddress}:/home/ec2-user"
         sh "ssh -o StrictHostKeyChecking=no ${username}@${ipAddress} ${command}"
     }
