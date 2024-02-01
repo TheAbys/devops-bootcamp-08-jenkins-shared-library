8b0a2572006929778f08dba48cf5c911099abe41
Sebastian Müller <sebastian.mueller@syskron.com>
2024-02-01 10:49:43 +0100

    updated to use bash script

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,9 @@
 #!/user/bin/env groovy
 
-def call(String username, String ipAddress, String command, String sshAgentCredentials = "ssh-agent-credentials") {
+def call(String username, String ipAddress, String imageName, String sshAgentCredentials = "ssh-agent-credentials") {
     sshagent(["${sshAgentCredentials}"]) {
+        sh "scp server-cmds.sh ${username}@${ipAddress}:/home/ec2-user"
         sh "scp docker-compose.yaml ${username}@${ipAddress}:/home/ec2-user"
-        sh "ssh -o StrictHostKeyChecking=no ${username}@${ipAddress} ${command}"
+        sh "ssh -o StrictHostKeyChecking=no ${username}@${ipAddress} bash ./server-cmds.sh ${imageName}"
     }
 }
